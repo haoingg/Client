b2190e43b1215dd0b3f49bd85913665d49b35ce9
haoingg <slovess1@naver.com>
2021-07-25 23:12:34 +0900

    [스타벅스] order 액티비티

---
+++ OrderActivity.kt
@@ -0,0 +1,13 @@
+package com.example.starbucks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+
+}
